90bb95714756bc7b488a0eb944ff01763143b0d1
Santiago Ramos <78809453+sRamosg2018@users.noreply.github.com>
2023-04-26 21:42:13 +0200

    Update Practica1.py

--- Blur_detector/Practica1.py
+++ Blur_detector/Practica1.py
@@ -1,89 +1,48 @@
+import pathlib
 from PIL import Image
 from tqdm import tqdm
-from time import sleep
+from os import listdir
+from os.path import isfile, join
 import json
-from tqdm import trange
-import os
 import shutil
+import glob
+from time import sleep
 import cv2
 import numpy as np
+import argparse
 
 
-def rotate():
-    with Image.open("Lissandra.jpg") as img:
-        img.rotate(45).show()
+def main():
+    image_folder = pathlib.Path("D:/URJC/AndroidStudioImagenes")
+    trash_folder = pathlib.Path("")
+    report = pathlib.Path("")
 
+    images_to_process = []
 
+    for ext in ["*.jpg", "*.jpeg", "*.png"]:
+        images_to_process.extend(image_folder.rglob(ext))
 
-def bar():
-    with tqdm(total=100) as pbar:
-        for i in range(50):
+    number_of_images = len(images_to_process)
+
+    print(number_of_images)
+    with tqdm(total=number_of_images) as pbar:
+        for image in images_to_process:
+            with Image.open(image).convert('RGB') as img:
+                if is_blurred(img)[0]:
+                    print("mandar a la carpeta basura")
+                else:
+                    print("mandar a la carpeta de imagenes")
             sleep(0.1)
-            pbar.update(2)
+            pbar.update(1)
 
 
 def is_blurred(image, threshold=90):
     imagecv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     graycv = cv2.cvtColor(imagecv, cv2.COLOR_BGR2GRAY)
     edgescv = cv2.Laplacian(graycv, cv2.CV_64F)
-
     variance = edgescv.var()
-
-    return variance < threshold, variance
-
-
-#    shutil.move("blurry_images/Blurry1.jpg", "image")
-
-def test_blur(): #JUST A TEST
-    with Image.open("Lissandra.jpg") as img:
-        blur = is_blurred(img)
-        if blur[0]:
-            print("Si esta borrosa")
-        else:
-            print("No está borrosa")
-        print(blur)
+    return (variance < threshold, variance)
 
 
-def blur_detector(*args):
-    if len(args)==2:
-        args0 = args[0].split('=')
-        args1 = args[1].split('=')
-
-        source = ' '.join(map(str, args0[1:2]))
-        destination = ' '.join(map(str, args1[1:2]))
-
-        print("Source: "+source)
-        print("Destination: "+destination)
-        isDirectory = (os.path.isdir(source) and os.path.isdir(destination))
-        if not isDirectory:
-            print((os.path.isdir(source)))
-            print((os.path.isdir(destination)))
-            print("Error con los parametros de entrada: " + source+','+destination)
-            exit();
-        else:
-            with tqdm(total=100) as pbar:
-                for file in os.listdir(source):
-                    with Image.open(source+"/"+file) as img:
-                        pbar.update(100 / len(os.listdir(source)))
-                        blurred = is_blurred(img)
-                        if blurred[0]:
-                            print("Archivo " + source + "/" + file + " movido a: " + destination)
-                            sleep(0.2)
-
-                          #  shutil.move(source+"/"+file, destination)
-                        >> > import os
-                        >> > filename, file_extension = os.path.splitext('/path/to/somefile.ext')
-                        >> > filename
-                        '/path/to/somefile'
-                        >> > file_extension
-                        '.ext
-                          # print(source, destination)
-
-
-
-
-    else:
-        print("Numero incorrecto de argumentos: --image_folder='(name of your source directory)', --trash_folder='(name of you destination directory)'")
-
-
-blur_detector("--image_folder=./image","--trash_folder=./blurry_images")
+if __name__ == "__main__":
+    main()
